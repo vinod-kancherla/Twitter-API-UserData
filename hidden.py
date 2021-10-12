510c939a2a7f69d198d080538810e390fe20e133
Vinod Kancherla <gj4865@wayne.edu>
2021-10-12 19:01:55 -0400

    Sort-3

--- hidden.py
+++ hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "h7Lu...Ng",
-            "consumer_secret": "dNKenAC3New...mmn7Q",
-            "token_key": "10185562-eibxCp9n2...P4GEQQOSGI",
-            "token_secret": "H0ycCFemmC4wyf1...qoIpBo"}
+    return {"consumer_key": "VspoKyOLGhBil2tO6HdUQA9t0",
+            "consumer_secret": "vVsbmLrcnWZaS8XhFBtAImbt1ODna5weT2ca1s1T9NBlgBtes8",
+            "token_key": "4126047553-lgiYq2sfxeMoX0i0v1polQrWfgeGgmwghAfXwf9",
+            "token_secret": "B5332Ujy2FWbxeKf3TRUEq2D3oYQud69kWeeMZHPlIXXl"}
